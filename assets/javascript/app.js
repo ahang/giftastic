64c835c6aa867ca875580563fa0f89ee16689a19
dashaman <hang.andy.t@gmail.com>
2017-03-05 18:12:01 -0600

    v0.03

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,18 @@ $(document).ready(function() {
     /////////////////////////////
     var marvelCharacters = ["Iron Man", "Spiderman", "Starlord", "Captain America", "Bruce Banner", "Thor", "Deadpool", "Wolverine", "Thanos", "Daredevil", "Doctor Strange", "Black Widow"];
 
-    $(".mCharacters").on("click", function() {
+    //displayMarvelInfo function will dispays the appropriate content in the character div
+    function displayMarvelInfo() {
         console.log("Clicked");
+        reset(); //emptys out the character div for each character button pressed
         var display = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + display + "rating=pg-13&limit=10&api_key=dc6zaTOxFJmzC";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=marvel+" + display + "&rating=pg-13&limit=10&api_key=dc6zaTOxFJmzC";
 
         //Creating AJAX call for each of the character buttons being click
         $.ajax({
             url: queryURL,
             method: "GET"
         }).done(function(response) {
-            debugger;
             //Checking to see JSON
             //$(".characters").html(JSON.stringify(response));
 
@@ -30,23 +31,23 @@ $(document).ready(function() {
             for (var i = 0; i < results.length; i++) {
                 if (results[i].rating !== "r") {
                     console.log("loop check");
-                    var marvelDiv = $("<div class='chars'>");
+                    var marvelDiv = $("<div class='chars'>"); //creating a div to hold the chars
 
                     var rating = results[i].rating; // storing the rating data
                     var pRate = $("<p>").text("Rating: " + rating); //creating an element with the result of the rating
 
-                    var marvelImage = $("<img>");
+                    var marvelImage = $("<img>"); //creating a img div
                     marvelImage.attr("src", results[i].images.fixed_height.url);
+                    //marvelImage.attr("src", results[i].images.fixed_height.url);
 
-                    marvelDiv.append(pRate);
-                    marvelDiv.append(marvelImage);
+                    marvelDiv.append(pRate); //appends the rating
+                    marvelDiv.append(marvelImage); //appends the image
 
-                    $(".characters").prepend(marvelDiv);
+                    $(".characters").prepend(marvelDiv); //Adding the gifs to the characters div
                 }
             }
         });
-    });
-
+    };
 
     //Creating a function to render the buttons
     function renderButtons() {
@@ -59,9 +60,14 @@ $(document).ready(function() {
             var charButton = $("<button>"); //creating a button
             charButton.addClass("mCharacters"); //adding character class
             charButton.attr("data-name", marvelCharacters[i]); //adding an attr of the data-name
+            console.log(charButton);
             charButton.text(marvelCharacters[i]); //adding the text to the button
             $(".marvel-view").append(charButton); //appending it to the div
         }
+    }
+    //emptys our the characters div
+    function reset() {
+        $(".characters").empty();
     }
 
     //adding on click to handle the submission of a new character
@@ -72,6 +78,21 @@ $(document).ready(function() {
         var marvelChar = $(".marvel-input").val().trim(); //grabbing the input and trimming out any extra whitespace
         marvelCharacters.push(marvelChar); //adds the input to the array
         renderButtons(); //re-renders the button with the newly added character
+    });
+
+    $(document).on("click", "button", displayMarvelInfo);
+
+    $(".gif").on("click", function() {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+             $(this).attr("src", $(this).attr("data-still"));
+             $(this).attr("data-state", "still");
+        }
     });
 
     //Rendering the button
